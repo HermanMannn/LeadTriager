8728cc27336c65acb4bdc083a54c0fafa8cdcda5
Nader Zamrawi <hermannnslol@gmail.com>
2026-06-03 13:06:19 +0400

    Delete testtttapi123.bat

--- testtttapi123.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting SynthAnalyst + Lead Triager...
-
-
-start "Lead Triager" cmd /k "cd /d %~dp0 && python main.py"
-start "ngrok" cmd /k "cd /d %~dp0 && ngrok http 5678"
-
-echo All services running.
